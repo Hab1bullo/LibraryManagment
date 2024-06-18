e4f575b4374a07e1fa2d651c67916f7b149745c4
Hab1bullo <xabibullotoyrilmasov385@gmail.com>
2024-06-19 00:19:22 +0500

    Order validation added

--- src/validation/book.valid.js
+++ src/validation/book.valid.js
@@ -1,9 +1,25 @@
 import Joi from "joi";
 
 
-export const bookvalid = () => {
+export const bookvalid = (body) => {
 
-    const schema = Joi.object({
-        
-    });
+    const bookSchema = Joi.object({
+        title: Joi.string().required(),
+        authorId: Joi.string().guid({ version: 'uuidv4' }).required(),
+        genreId: Joi.string().guid({ version: 'uuidv4' }).required(),
+        price: Joi.number().precision(2).positive().required(),
+        stock: Joi.number().integer().required(),
+        publishedDate: Joi.date().iso().required(),
+        status: Joi.string().valid('available', 'out of stock', 'discontinued').required(),
+        imageUrls: Joi.array().items(Joi.string().uri()).required(),
+        description: Joi.string().required()
+      });
+
+      const { error, value } = orderSchema.validate(body);
+
+    if (error) {
+        throw error.details;
+    } else {
+        return value
+    }
 }
---
+++ src/validation/order.valid.js
@@ -0,0 +1,27 @@
+import Joi from 'joi';
+
+
+export const orderValid = (body) => {
+
+    const orderSchema = Joi.object({
+        userId: Joi.string().guid({ version: 'uuidv4' }).required(),
+        items: Joi.array().items(
+            Joi.object({
+                bookId: Joi.string().guid({ version: 'uuidv4' }).required(),
+                quantity: Joi.number().integer().positive().required()
+            })
+        ).required(),
+        totalPrice: Joi.number().precision(2).positive().required(),
+        status: Joi.string().valid('pending', 'completed', 'canceled').required(),
+    });
+
+
+    const { error, value } = orderSchema.validate(body);
+
+    if (error) {
+        throw error.details;
+    } else {
+        return value
+    }
+
+}
